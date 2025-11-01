e49e9a659fdfc7c18771743b4264ec569dde37f7
Ernest Mallett <ernest.mallett@dccoder.com>
2025-11-01 02:01:56 -0500

    Update projects page

---
+++ src/projects.ts
@@ -0,0 +1,11 @@
+
+export const projects = [
+  {
+    name: 'My Awesome Project',
+    link: 'https://github.com/dccoder90/my-awesome-project',
+    description: 'This is a description of my awesome project. It does awesome things and is built with awesome technologies.',
+    image: '/imgs/posts/pexels-simon73-1183099.jpg',
+    startDate: '10/2023',
+    endDate: '12/2023',
+  },
+];
