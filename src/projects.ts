e1d3662c6e5ad29907657e32f338887037a0a3b3
Ernest Mallett <ernest.mallett@dccoder.com>
2025-11-01 02:45:10 -0500

    Update projects page

--- src/projects.ts
+++ src/projects.ts
@@ -1,6 +1,24 @@
 
+export const projectsPerRow = 3;
+
 export const projects = [
   {
+    name: 'My Awesome Project',
+    description: 'This is a description of my awesome project. It does awesome things and is built with awesome technologies.',
+    // image: '/imgs/posts/pexels-simon73-1183099.jpg',
+    // link: 'https://github.com/dccoder90/my-awesome-project',
+    startDate: '10/2023',
+    // endDate: '12/2023',
+    tags: ['JavaScript', 'Web Development', 'Awesome'],
+  },  {
+    name: 'My Awesome Project',
+    description: 'This is a description of my awesome project. It does awesome things and is built with awesome technologies.',
+    // image: '/imgs/posts/pexels-simon73-1183099.jpg',
+    // link: 'https://github.com/dccoder90/my-awesome-project',
+    startDate: '10/2023',
+    // endDate: '12/2023',
+    tags: ['JavaScript', 'Web Development', 'Awesome'],
+  }, {
     name: 'My Awesome Project',
     description: 'This is a description of my awesome project. It does awesome things and is built with awesome technologies.',
     // image: '/imgs/posts/pexels-simon73-1183099.jpg',
