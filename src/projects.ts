a75cc909ed61e044a3327f788c6b747588f04a36
Ernest Mallett <ernest.mallett@dccoder.com>
2025-11-02 14:02:39 -0600

    Projects update

--- src/projects.ts
+++ src/projects.ts
@@ -2,17 +2,26 @@
 export const projectsPerRow = 3;
 
 export const projects = [
+    {
+    name: 'This Website!',
+    description: "My personal website using Astro, featuring a modern design, light/dark mode, and more. It's hosted on GitHub Pages and the codebase is open source.",
+    link: 'https://github.com/DCCoder90/dccoder90.github.io',
+    startDate: '10/2025',
+    endDate: '11/2025',
+    image: '/imgs/projects/my-site.jpg',
+    tags: ['JavaScript', 'JSON', 'DevOps', 'CSS'],
+  },
   {
     name: 'mtg-mcp',
     description: 'This is a simple MCP server for the Scryfall to assist with card data and deck building. This allows clients such as Claude Desktop to search for Magic: The Gathering card details using the Scryfall API.',
      link: 'https://github.com/DCCoder90/mtg-mcp',
-    startDate: '10/2026',
+    startDate: '10/2025',
     tags: ['AI', 'MCP', 'Golang'],
   },  {
     name: 'DEngine',
     description: 'dEngine is a simple 2D game engine written in C++ and making use of the SDL library. This is really just kind of a hobby and nothing amazing is expected to come out of this aside from maybe a simple game or two I write just because.',
     link: 'https://github.com/DCCoder90/dengine',
-    startDate: '07/2026',
+    startDate: '07/2025',
     tags: ['C++', 'SDL', 'zLib'],
   }, {
     name: 'Aries Cloud Migration',
@@ -36,7 +45,7 @@ export const projects = [
     link: 'https://www.facebook.com/editorialmanager/posts/our-upcoming-multi-phase-migration-to-the-aws-cloud-will-enable-aries-to-scale-i/10158281955922407/',
     startDate: '03/2019',
     endDate: '02/2020',
-    tags: ['Blazor', 'C#', 'AWS', 'DevOps', 'JavaScript', 'HTML', 'CSS', 'Xamarin'],
+    tags: ['Blazor', 'C#', 'AWS', 'DevOps', 'JavaScript', 'HTML', 'CSS', 'Xamarin', 'Mobile'],
   },{
     name: 'EcoCards',
     description: 'EcoCards was created with the goal of saving resources while raising money and awareness for charities located around the world. Almost 7 billion paper cards are given each year in the U.S. Imagine saving the money you would have spent on cards and stamps while helping the environment and donating to charities just by sending an EcoCard.',
@@ -57,12 +66,12 @@ export const projects = [
     description: 'With Discover Brewton we give you an insight into the small town of Brewton, Alabama. With a population of 5,408, the city is the county seat of Escambia County located in south central Alabama, just north of the Florida Panhandle. ',
     startDate: '05/2018',
     endDate: '05/2018',
-    tags: ['C#', '.Net Standard', 'Nuget', 'JSON', 'Xamarin', 'Mobile' ],
+    tags: ['C#', '.Net Standard', 'JSON', 'Xamarin', 'Mobile' ],
   },{
     name: 'Provalus Bootcamp',
     description: 'With the assistance of one of my colleges, I set up and developed the structure of the Provalus developer boot-camp. This was an 8 week course that taught individuals with no prior experience the fundamentals of C#, ASP.NET, CSS, and HTML. My work consisted of utilizing Pluralsight as a supplementary tool with a primary focus on hands on learning and lectures with a relaxed environment.',
     startDate: '07/2017',
     endDate: '10/2017',
-    tags: ['C#', 'CSS', '.Net Framework', 'HTML', 'JSON', 'Javascript', 'SQL' ],
+    tags: ['C#', 'CSS', '.Net Framework', 'HTML', 'JSON', 'JavaScript', 'SQL' ],
   },
 ];
